4c1d4caf7b876d04a921d19570977ad287cdb413
Ahmed Helmy <115832929+5elmy@users.noreply.github.com>
2024-12-30 00:05:29 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import { app, server } from "./lib/socket.io.js"
 
 dotenv.config()
 const port = process.env.PORT
- app.use(cors({origin:"http://localhost:5173" , credentials:true}))
+ app.use(cors({origin:"*", credentials:true}))
+//  app.use(cors({origin:["http://localhost:5173","https://vibe-chat-frontend.vercel.app"] , credentials:true}))
  app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser()) 
--- src/lib/socket.io.js
+++ src/lib/socket.io.js
@@ -10,7 +10,8 @@ const server = http.createServer(app)
 
 const io= new Server(server , {
     cors:{
-        origin:["http://localhost:5173","https://vibe-chat-frontend.vercel.app"]
+        origin:"*"
+        // origin:["http://localhost:5173","https://vibe-chat-frontend.vercel.app"]
     }
 })
 
