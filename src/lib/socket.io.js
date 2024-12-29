cc442d9179c8ac9c82316b6b350eaae11ff416dc
Ahmed Helmy <115832929+5elmy@users.noreply.github.com>
2024-12-29 23:39:21 +0200

    first commit

--- src/lib/socket.io.js
+++ src/lib/socket.io.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io= new Server(server , {
     cors:{
-        origin:["http://localhost:5173"]
+        origin:["http://localhost:5173","https://vibe-chat-frontend.vercel.app"]
     }
 })
 
@@ -29,6 +29,11 @@ io.on("connection" , (socket)=>{
     if(userId) userSocketMap[userId]= socket.id
         // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers",Object.keys(userSocketMap))
+
+
+
+
+
     socket.on("disconnect", ()=>{
         
         console.log("A user disconnected", socket.id);
